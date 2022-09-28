14436b75e4670f47c50e4b80ca6474d1a701ef5f
RuslanBeresnev <rusber03102003@gmail.com>
2022-09-28 23:33:13 +0300

    Реализована задача про ILazy, а также тесты к ней

---
+++ Задания с 14.09.2022/Task1/Task1/ILazy.cs
@@ -0,0 +1,12 @@
+﻿namespace ILazy;
+
+/// <summary>
+/// Интерфейс для реализации ленивых вычислений (ха-ха, какие же они ленивые ...)
+/// </summary>
+public interface ILazy<T>
+{
+    /// <summary>
+    /// Получить значение вычисления
+    /// </summary>
+    public T? Get();
+}
---
+++ Задания с 14.09.2022/Task1/Task1/MultiLazy.cs
@@ -0,0 +1,40 @@
+﻿namespace ILazy;
+
+/// <summary>
+/// Многопоточная реализация ленивых вычислений
+/// </summary>
+public class MultiLazy<T> : ILazy<T>
+{
+    private Func<T>? function;
+    private T? value;
+    private volatile bool valueCalculated = false;
+    private Object lockObject = new();
+
+    public MultiLazy(Func<T> function)
+    {
+        if (function == null)
+        {
+            throw new InvalidOperationException("В качестве лямбда-функции передана нулевая функция");
+        }
+        this.function = function;
+    }
+
+    public T? Get()
+    {
+        if (valueCalculated)
+        {
+            return value;
+        }
+
+        lock (lockObject)
+        {
+            if (!valueCalculated)
+            {
+                value = function!();
+                function = null;
+                valueCalculated = true;
+            }
+            return value;
+        }
+    }
+}
