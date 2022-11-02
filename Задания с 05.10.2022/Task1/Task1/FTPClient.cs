825c10f9f2f0aa2b995e406b40e557a6940146b5
RuslanBeresnev <rusber03102003@gmail.com>
2022-11-02 23:17:33 +0300

    Частичная реализация

---
+++ Задания с 05.10.2022/Task1/Task1/FTPClient.cs
@@ -0,0 +1,33 @@
+﻿namespace SimpleFTP;
+
+using System.Net;
+using System.Net.Sockets;
+
+/// <summary>
+/// Realization of FTP-client
+/// </summary>
+public class FTPClient
+{
+    private int port;
+    private IPAddress ip;
+
+    public FTPClient(int port, IPAddress ip)
+    {
+        this.port = port;
+        this.ip = ip;
+
+        StartRequestsAccepting();
+    }
+
+    /// <summary>
+    /// Start accepting requests from user
+    /// </summary>
+    public void StartRequestsAccepting()
+    {
+        while (true)
+        {
+            var request = Console.ReadLine();
+            // pass
+        }
+    }
+}
---
+++ Задания с 05.10.2022/Task1/Task1/FTPServer.cs
@@ -0,0 +1,49 @@
+﻿namespace SimpleFTP;
+
+using System.Net;
+using System.Net.Sockets;
+
+/// <summary>
+/// FTP-server realization
+/// </summary>
+public class FTPServer
+{
+    private int port;
+    private IPAddress ip;
+
+    public FTPServer(int port, IPAddress ip)
+    {
+        this.port = port;
+        this.ip = ip;
+
+        StartServer();
+    }
+
+    private async void StartServer()
+    {
+        var listener = new TcpListener(ip, port);
+        listener.Start();
+        var socket = await listener.AcceptSocketAsync();
+
+        Task.Run(async () =>
+        {
+            // pass
+        });
+    }
+
+    /// <summary>
+    /// Server's directory enumeration realization
+    /// </summary>
+    public void PerformListRequest()
+    {
+        // pass
+    }
+
+    /// <summary>
+    /// Get file from server's directory
+    /// </summary>
+    public void PerformGetRequest()
+    {
+        // pass
+    }
+}
