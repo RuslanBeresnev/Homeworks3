582f8dd6a6ee894be16b6f7d1f385d41cc7bd508
RuslanBeresnev <rusber03102003@gmail.com>
2022-12-14 12:58:08 +0300

    Задача, выполненная без тестов

---
+++ Control Work 2/Task1/Task1/ConsoleChatFunctions.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleChat;
+
+using System.Net.Sockets;
+
+/// <summary>
+/// Realization of general functions as message sending or message receiving or stop chating function
+/// </summary>
+public class ConsoleChatFunctions
+{
+    protected CancellationTokenSource cancellationTokenSource = new();
+
+    /// <summary>
+    /// Message sending realization
+    /// </summary>
+    protected void Writer(NetworkStream stream)
+    {
+        Task.Run(async () =>
+        {
+            var streamWriter = new StreamWriter(stream) { AutoFlush = true };
+            while (!cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                var messageToSend = Console.ReadLine();
+                await streamWriter.WriteLineAsync(messageToSend);
+            }
+
+            await streamWriter.WriteLineAsync("exit");
+            streamWriter.Close();
+        });
+    }
+
+    /// <summary>
+    /// Message receiving realization
+    /// </summary>
+    protected void Reader(NetworkStream stream)
+    {
+        Task.Run(async () =>
+        {
+            var streamReader = new StreamReader(stream);
+            while (!cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                var receivedMessage = await streamReader.ReadLineAsync();
+                if (receivedMessage == "exit")
+                {
+                    StopChating();
+                    break;
+                }
+                Console.WriteLine(receivedMessage);
+            }
+
+            streamReader.Close();
+        });
+    }
+
+    /// <summary>
+    /// Stops chating
+    /// </summary>
+    public void StopChating()
+    {
+        cancellationTokenSource.Cancel();
+    }
+}
---
+++ Control Work 2/Task1/Task1/Server.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleChat;
+
+using System.Net;
+using System.Net.Sockets;
+
+/// <summary>
+/// Console chat server realization
+/// </summary>
+public class Server : ConsoleChatFunctions
+{
+    private TcpListener listener;
+
+    public Server(int port)
+    {
+        listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+    }
+
+    /// <summary>
+    /// Starts the server
+    /// </summary>
+    public async void Start()
+    {
+        listener.Start();
+        Console.WriteLine("Server starts. Getting connection with client ...");
+        while (!cancellationTokenSource.Token.IsCancellationRequested)
+        {
+            var client = await listener.AcceptTcpClientAsync();
+            Console.WriteLine("Connection established");
+            Writer(client.GetStream());
+            Reader(client.GetStream());
+        }
+        listener.Stop();
+    }
+}
